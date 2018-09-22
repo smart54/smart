8a0e38ef052bac921d1ef14cc1e168d14224e097
Admin <dev@stark-media.ru>
2018-09-23 00:50:59 +0700

    Initial commit

---
+++ resources/views/template/script.blade.php
@@ -0,0 +1,18 @@
+<script>
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: 'Привет, Vue!'
+        }
+
+    });
+
+    var vie = new Vue({
+        el: '#text-vie',
+        data: {
+            message: 'Просто текст'
+        }
+    })
+
+
+</script>
